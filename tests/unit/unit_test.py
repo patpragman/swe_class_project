50a1d3e51f3723b4deded926e744850806390ebe
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-09-06 22:45:17 -0800

    tinkering with CI/CD stuff

--- tests/unit/unit_test.py
+++ tests/unit/unit_test.py
@@ -1,9 +1,12 @@
 """
 unit tests for all the appropriate classes in the app go here
 """
-print("Running Unit Tests!")
 import sys
+import os
 
+print("Running Unit Tests!")
+print('running from:')
+print(os.getcwd())
 # check that the app module imports at all
 import app
 assert "app" in sys.modules
