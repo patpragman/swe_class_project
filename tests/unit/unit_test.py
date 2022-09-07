2b8a2e210eec280f316a4e1e96abda64cff49014
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-09-06 22:59:40 -0800

    fixing import stuff

--- tests/unit/unit_test.py
+++ tests/unit/unit_test.py
@@ -6,7 +6,13 @@ import os
 
 print("Running Unit Tests!")
 print('running from:')
-print(os.getcwd())
+
 # check that the app module imports at all
-import app
-assert "app" in sys.modules
+from app.model import Model
+from app.view import View
+from app.controller import Controller
+
+for file_name in os.listdir(f"{os.getcwd()}/app"):
+    module_name = f"app.{file_name.replace('.py', '')}"
+    if "__" not in file_name:
+        assert module_name in sys.modules
