45e22a8d997fc61fd60983807cde9c68a76d5bfb
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-09-24 22:07:07 -0800

    wrote bash script to package the post lambda function

---
+++ deployment/package_post.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Package for Deployment!
+# Generate a Zip File of the post lambda functions
+
+mkdir "deployment_package"
+
+# copy all the lambda functions into the deployment package
+cp lambda_functions/dev-post/* "deployment_package"
+# build a venv of all the tools we need
+cd deployment_package
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
+# zip up the packages you'll need for the lambda
+cd venv/lib/python3.10/site-packages
+zip -r ../../../../../deployment_package.zip .
+cd ../../../../
+zip -g ../deployment_package.zip lambda_function.py
+cd ..
+rm -rf deployment_package
+
