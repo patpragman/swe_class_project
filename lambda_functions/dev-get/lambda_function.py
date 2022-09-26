ada1789e168cb21225fb82be26c49338bf138214
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-09-26 14:28:43 -0800

    marching orders

--- lambda_functions/dev-get/lambda_function.py
+++ lambda_functions/dev-get/lambda_function.py
@@ -3,6 +3,11 @@ import json
 
 def lambda_handler(event, context):
     #  Code to serve the front end goes here
+
+    # a request for "home" or to initially load the app comes in, and we send back HTML files form S3 that have
+    # the website in them
+
+
     return {
         'statusCode': 200,
         "headers": {'Content-Type': 'text/html'},
