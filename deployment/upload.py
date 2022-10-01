9e22a5fbfc39ccf04035b7780a75a2d25509e2a9
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-09-30 22:24:49 -0800

    testing out new content deployment pipeline

---
+++ deployment/upload.py
@@ -0,0 +1,53 @@
+"""
+This code is a series of functions to push code and content to AWS
+
+"""
+
+import boto3
+import os
+
+def update_api(lambda_function_endpoints=["post", "get"], env="dev"):
+    """
+    This code updates the api by spooling up a lambda client, then for each end point selected
+    uploading the associated zip file.
+
+    notes.  The zip files have to have a specific format "deployment_package_<type of api action>.zip"
+    we could probably make this more generalized later, refactor if necessary
+
+
+    :param lambda_function_endpoints: the api endpoint names - for instance, <env>-post or somethign
+    :param env: the other half of the api endpoint name
+    :return: None - it just does it's work and then dies
+    """
+    lambda_client = boto3.client("lambda")
+    for lambda_function_endpoint in lambda_function_endpoints:
+        # iterate through post and get and update the lambda functions that handle posts and get requests on aws
+        with open(f"deployment_package_{lambda_function_endpoint}.zip", "rb") as zipfile:
+            lambda_client.update_function_code(
+                FunctionName=f'{env}-{lambda_function_endpoint}',
+                ZipFile=zipfile.read()
+            )
+
+
+def update_web_content(folderpath="web_content", env="dev"):
+    """
+    This code takes all the content of the web content folder, and uploads it to the applicable S3 bucket
+    path and all
+    :param path: the name of the folder or path to the folder containing the web content.
+    :param env: the env referenced in the bucket, such as swe.class.project.dev or something similar
+    :return: None - it does it's work then quietly dies.
+    """
+
+    s3_client = boto3.resource("s3", region_name="us-west-2")
+    bucket_name = f"swe.class.project.{env}"
+    selected_bucket = s3_client.Bucket(bucket_name)
+
+    # this code modified from stack exchange, but yeah, walk through directories and create folders as required
+    for path, subdirs, files in os.walk(folderpath):
+        path = path.replace("\\", "/")
+        directory_name = path.replace(path, "")
+        for file in files:
+            print(file)
+            selected_bucket.upload_file(os.path.join(path, file), directory_name + '/' + file)
+
+
--- lambda_functions/dev_get/lambda_function.py
+++ lambda_functions/dev_get/lambda_function.py
@@ -1,15 +1,22 @@
 import json
-
-
+import boto3
+ENV = "dev"
 def lambda_handler(event, context):
     #  Code to serve the front end goes here
 
-    # a request for "home" or to initially load the app comes in, and we send back HTML files form S3 that have
-    # the website in them
+    # this just goes to s3, and sends the main page for now, something more complicated might come along later
+    # we'll need it to process the get request and send the appropriate files from the s3 bucket
 
+    s3_client = boto3.resource("s3", region_name="us-west-2")
+    bucket_name = f"swe.class.project.{ENV}"
+    selected_bucket = s3_client.Bucket(bucket_name)
+
+    # so honestly, this just fetches one page... that's the only thing it does naturally more to come
+    obj = s3_client.Object(bucket_name, "/main.html")
+    content = obj.get()['Body'].read().decode('utf-8')
 
     return {
         'statusCode': 200,
         "headers": {'Content-Type': 'text/html'},
-        'body': json.dumps('<h3>Here we go! Hello World!!! WOOO HOO! Welcome!</h3>')
+        'body': json.dumps(content)
     }
