8fc7c116062bc857f207398eef5dbd0527e42ef2
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-01 19:59:28 -0800

    more decoding... painful

--- test/local_testing/scratch.py
+++ test/local_testing/scratch.py
@@ -1,3 +1,4 @@
+import base64
 wtf = {'version': '1.0',
        'resource': '/dev-get',
        'path': '/default/dev-get',
@@ -25,4 +26,6 @@ wtf = {'version': '1.0',
            {'type': 'image?path="/main.html"'},
        'multiValueQueryStringParameters': {'type': ['image?path="/main.html"']}, 'requestContext': {'accountId': '545455113134', 'apiId': 'botp4w3ryj', 'domainName': 'botp4w3ryj.execute-api.us-west-2.amazonaws.com', 'domainPrefix': 'botp4w3ryj', 'extendedRequestId': 'ZWtClgA0PHcEPiw=', 'httpMethod': 'GET', 'identity': {'accessKey': None, 'accountId': None, 'caller': None, 'cognitoAmr': None, 'cognitoAuthenticationProvider': None, 'cognitoAuthenticationType': None, 'cognitoIdentityId': None, 'cognitoIdentityPoolId': None, 'principalOrgId': None, 'sourceIp': '24.237.81.149', 'user': None, 'userAgent': 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0', 'userArn': None}, 'path': '/default/dev-get', 'protocol': 'HTTP/1.1', 'requestId': 'ZWtClgA0PHcEPiw=', 'requestTime': '02/Oct/2022:02:19:47 +0000', 'requestTimeEpoch': 1664677187585, 'resourceId': 'ANY /dev-get', 'resourcePath': '/dev-get', 'stage': 'default'}, 'pathParameters': None, 'stageVariables': None, 'body': None, 'isBase64Encoded': False}
 
-print(wtf["queryStringParameters"]['type'])
+
+with open("../../web_content/images/uaastudents.jpg", "rb") as jpg_file:
+    print(base64.b64encode(jpg_file.read()).decode("utf-8"))
