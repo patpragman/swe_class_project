5ecb459b98bb4f795534877a2807a3c2f13db888
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-01 18:55:58 -0800

    quick change for typo

--- lambda_functions/dev_get/lambda_function.py
+++ lambda_functions/dev_get/lambda_function.py
@@ -9,7 +9,7 @@ SUPPORTED_IMAGE_FILETYPES = ["png", "jpeg", "jpg", "gif"]
 # build a dictionary of HTTP headers
 header_mapping = {"html": {'Content-Type': 'text/html'}}
 for image_type in SUPPORTED_IMAGE_FILETYPES:
-    image_type = "jpeg" if image_type == "jpeg" else image_type
+    image_type = "jpeg" if image_type == "jpg" else image_type
     header_mapping[image_type] = {'Content-Type': f'image/{image_type}'}
 
 
