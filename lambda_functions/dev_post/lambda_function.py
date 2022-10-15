e000bce4ad3573721fd7ac9dcd473fd1909308a7
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-15 13:28:07 -0800

    robostified api a little bit, added error handling and some response codes, etc.

--- lambda_functions/dev_post/lambda_function.py
+++ lambda_functions/dev_post/lambda_function.py
@@ -19,32 +19,54 @@ def create_flashcard(payload: dict) -> dict:
 
 
 def lambda_handler(event, context):
-    '''Provide an event that contains the following keys:
-
+    '''
+    Provide an event that contains the following keys:
       - operation: one of the operations in the operations dict below
       - payload: a parameter to pass to the operation being performed
     '''
-    event = json.loads(event['body'])
-    operation = event['operation']
 
-    # define more operations in here - echo is the "hello world" of this sort of thing...
-    operations = {
-        'echo': lambda x: x,  # echos back literally all data you send
-        'create_flashcard': lambda payload: create_flashcard(payload)  # sends back a dictionary
+    response = {
+        "statusCode": 500,  # start with the assumption that we broke something
+        "body": {
+            "success": False,
+            "return_payload": {
+                "message": "unexplained server error"
+            }
+        },
     }
 
-    if operation in operations:
-        # a simple "200" request, but suffice it to say the operation worked.
-        response = {
-            "statusCode": 200,
-            "body": "[]",
-        }
+    # this code is the wiring between the json sent as post request and the functions we have in our app that does stuff
+    operations = {
+        'echo': lambda x: {"success": True,
+                           "return_payload": {"message": x}
+                           },  # echos back literally all data you send
+        'create_flashcard':
+            lambda payload: create_flashcard(payload)  # creates a flashcard and sends back confirmation
+    }
 
-        # get the result, stick that in "body"
-        result = operations[operation](event.get('payload'))
-        response['body'] = result
-        # and send it back!
-        return response
-    else:
-        # we can handle "bad operations" or something later
-        raise ValueError('Unrecognized operation "{}"'.format(operation))
+    try:
+        # we wrap all of this in a try/except block to catch any and all errors - no matter what we want to control
+        # the output of the lambda function
+        event = json.loads(event['body'])
+        operation = event['operation']
+
+        # check if this operation is supported, then run the operation
+        if operation in operations:
+            # a simple "200" request, but suffice it to say the operation worked.
+            response['statusCode'] = 200
+
+            # get the result, stick that in "body"
+            result = operations[operation](event.get('payload'))
+            response['body'] = result
+        else:
+            response['statusCode'] = 400
+            response['body']['return_payload']['message'] = f"the api operation: {operation} is not recognized"
+    except Exception as err:
+        """
+        if any sort of error happened while doing this, let's send back a response that indicates that there was an
+        internal server error
+        """
+        response['statusCode'] = 500
+        response['body']['return_payload']['message'] = f"received the following error during operations: \n {str(err)}"
+
+    return response
--- test/local_testing/local_post_test.py
+++ test/local_testing/local_post_test.py
@@ -7,10 +7,20 @@ print(url)
 test_code = {"key1": "value1",
              "key2": "value2",
              "key3": "value3",
-             "operation": "say_hello",
-             "payload": "patpatpatpatpat!"
+             "operation": "create_flashcard",
+             "payload": "test!"
              }
 
-x = requests.post(url, json=test_code)
+# testing the response codes for various operations
+test_operations = ["create_flashcard",
+                   "echo",
+                   "bad_payload!"]
+desired_responses = [
+    200, 200, 400
+]
+for k,v in zip(test_operations):
+    test_code["operation"] = k
+    post_request = requests.post(url, json=test_code)
+    print(post_request.request)
+    print(post_request.text)
 
-print(x, x.text)
