beb92a83e1839f612fce015e294453e50ae33404
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-15 13:48:17 -0800

    integration tests mocked up

--- test/local_testing/local_post_test.py
+++ test/local_testing/local_post_test.py
@@ -1,9 +1,13 @@
+"""
+hacked togehter local post request testing script to test the lambda function
+
+"""
+
 import json
 import requests
 from os import environ
 
 url = environ['AWS_URL_ENDPOINT']
-print(url)
 test_code = {"key1": "value1",
              "key2": "value2",
              "key3": "value3",
@@ -18,9 +22,21 @@ test_operations = ["create_flashcard",
 desired_responses = [
     200, 200, 400
 ]
-for k,v in zip(test_operations):
-    test_code["operation"] = k
-    post_request = requests.post(url, json=test_code)
-    print(post_request.request)
-    print(post_request.text)
+desired_api_state = [
+    False,
+    True,
+    False
+]
+
+try:
+    for k, v, d in list(zip(test_operations, desired_responses, desired_api_state)):
+        test_code["operation"] = k
+
+        post_request = requests.post(url, json=test_code)
+        assert post_request.status_code == v
+        response_data = json.loads(post_request.text)
+        assert response_data['success'] == d
+except AssertionError as err:
+    print(err)
+    print(k, v)
 
