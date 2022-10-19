b9f09192c3c1c6a3babdd37cf94b668269b9e48b
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-18 19:39:54 -0800

    delete_all.py adjustment

--- delete_all.py
+++ delete_all.py
@@ -2,6 +2,7 @@ import boto3
 import json
 
 if __name__ == "__main__":
+    # run this script at your own risk - deletes everything in s3......be afraid
     s3_client = boto3.resource("s3", region_name="us-west-2")
 
     s3_client.Bucket("swe.class.project.storage").put_object(Body=json.dumps([]), Key="users.json",
