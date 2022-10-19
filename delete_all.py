f1f1636713c9cfded6bb4fca4ecb5516688f27e2
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-18 19:33:05 -0800

    pep 8

--- delete_all.py
+++ delete_all.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     s3_client = boto3.resource("s3", region_name="us-west-2")
 
     s3_client.Bucket("swe.class.project.storage").put_object(Body=json.dumps([]), Key="users.json",
-                                                     ContentType='json')
+                                                             ContentType='json')
 
     s3_client.Bucket("swe.class.project.storage").put_object(Body=json.dumps([]), Key="card_list.json",
                                                              ContentType='json')
