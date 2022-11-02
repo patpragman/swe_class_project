a0b92e87f7766ef53d5c45a0969d2bdf635d2d7e
edsoer <ed@DESKTOP-CRFKM1L.localdomain>
2022-11-02 12:52:02 -0800

    refactored delete functions to work from payload. Separated delete user and delete flashcard functionality

--- lambda_functions/dev_post/delete.py
+++ lambda_functions/dev_post/delete.py
@@ -1,42 +1,84 @@
 import json
 import boto3
 from aws_config import STORAGE_BUCKET_NAME, REGION_NAME
-from retrieve import get_all_cards_as_list
+from retrieve import get_all_cards_as_list, get_all_users_as_list
 
 
-def delete_item_by_id(type_of_item, id) -> dict:
+def delete_flashcard(payload) -> dict:
 
-    if type_of_item == "flashcard":
+    # read card id from payload
+    id = int(payload['id'])
 
-        card_list = get_all_cards_as_list()
+    # check card list for matching id and pop
+    card_list = get_all_cards_as_list()
+    try:
+        match = False
+        for (i, card) in enumerate(card_list):
+            if int(card['id']) == id:
+                match = True
+                card_list.pop(i)
+        if not match:
+            raise IndexError
+    except IndexError('card id not found'):
+        return {'success': False, \
+        "message": f"unable to find card with matching id {id}", \
+            'objects': card_list}
 
-        try:
-            match = False
-            for (i, card) in enumerate(card_list):
-                if int(card['id']) == id:
-                    match = True
-                    card_list.pop(i)
-            if not match:
-                raise IndexError
-        except IndexError('card id not found'):
-            return {'success': False, \
-            "message": 'card id does not exist in user card list', \
-                'objects': card_list}
-
-        # now we write card_list back to s3
-        s3_client = boto3.resource("s3", region_name=REGION_NAME)
-        s3_client.Bucket(STORAGE_BUCKET_NAME).put_object(Body=json.dumps(card_list), Key='card_list.json',
-                                                    ContentType='json')
-        # return success code
-        return {'success': True, \
-            'message': f"card {id} successfully updated", \
-                'objects': card_list}
+    # write card_list back to s3
+    s3_client = boto3.resource("s3", region_name=REGION_NAME)
+    s3_client.Bucket(STORAGE_BUCKET_NAME).put_object(Body=json.dumps(card_list), Key='card_list.json',
+                                                ContentType='json')
+    # return success code
+    return {'success': True, \
+        'message': f"card {id} successfully deleted", \
+            'objects': card_list}
 
 
-    # branch for user deletion
-    return {"success": False,
-            "return_payload": {
-                "message": f"unable to fetch card id",
-                "object": []
-            }
-            }
+def delete_user(payload):
+
+    # get username from payload
+    username = payload['username']
+
+    # double check user exists
+    user_list = get_all_users_as_list()
+    user_idx = 0
+    exists = False
+    for (i, user) in enumerate(user_list):
+        if user['username'] == username:
+            exists = True
+            user_idx = i
+
+    if not exists:
+        raise Exception('no user with matching id found')
+
+    # if user exists, delete all that user's cards
+    card_list = get_all_cards_as_list()
+    cards_deleted = 0
+    for (i, card) in enumerate(card_list):
+        if card['username'] == username:
+            card_list.pop(i)
+            cards_deleted += 1
+
+    # now delete user
+    user_list.pop(user_idx)
+
+    # write card_list back to s3
+    s3_client = boto3.resource("s3", region_name=REGION_NAME)
+    s3_client.Bucket(STORAGE_BUCKET_NAME).put_object(Body=json.dumps(user_list), Key='user_list.json',
+                                                ContentType='json')
+    # return success code
+    return {'success': True, \
+        'message': f"user {username} successfully deleted. {cards_deleted} cards were removed.", \
+            'objects': user_list}
+
+    
+
+        
+
+
+
+        
+            
+
+
+
