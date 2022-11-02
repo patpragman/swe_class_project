79eb0e49fc1f06224861120c9e24bfb93f9dcd3c
Patrick Pragman <pat@pragman.io>
2022-11-01 16:06:40 -0800

    fixed packaging issue

--- lambda_functions/dev_post/update.py
+++ lambda_functions/dev_post/update.py
@@ -26,27 +26,19 @@ def update_card_by_id(payload: dict) -> dict:
         if not match:
             raise IndexError
     except IndexError:
-        return {'success': False, \
-            "message": 'card id does not exist in user card list', \
-                'objects': card_list}
+        return {'success': False,
+                'return_payload': {
+                    "message": 'card id does not exist in user card list',
+                    'return_payload': card_list}
+                }
 
     # now we write card_list back to s3
     s3_client = boto3.resource("s3", region_name=REGION_NAME)
     s3_client.Bucket(STORAGE_BUCKET_NAME).put_object(Body=json.dumps(card_list), Key='card_list.json',
-                                                    ContentType='json')
-    
-    return {'success': True, \
-        'message': f"card {card_id} successfully updated", \
-            'objects': card_list}
+                                                     ContentType='json')
 
-
-
-
-
-
-    return {"success": False,
+    return {'success': True,
             "return_payload": {
-                "message": f"not implemented yet",
-                "objects": get_all_cards_by_user_as_list(username)
-            }
+                'message': f"card {card_id} successfully updated", \
+                'objects': card_list}
             }
