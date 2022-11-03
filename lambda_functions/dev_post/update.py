eff31ab90d50c999f0ac82aa267f9cdeea8643c5
Patrick Pragman <pat@pragman.io>
2022-11-03 13:44:13 -0800

    fixed return payload

--- lambda_functions/dev_post/update.py
+++ lambda_functions/dev_post/update.py
@@ -15,7 +15,6 @@ def update_card_by_id(payload: dict) -> dict:
 
     updated_card = payload['object']
 
-
     card_list = get_all_cards_by_user_as_list(username)
 
     try:
@@ -27,16 +26,21 @@ def update_card_by_id(payload: dict) -> dict:
         if not match:
             raise IndexError
     except IndexError:
-        return {'success': False, \
-            "message": 'card id does not exist in user card list', \
-                'objects': card_list}
+
+        return {'success': False,
+                "return_payload": {
+                    "message": 'card id does not exist in user card list', \
+                    'objects': card_list}}
 
     # now we write card_list back to s3
     s3_client = boto3.resource("s3", region_name=REGION_NAME)
     s3_client.Bucket(STORAGE_BUCKET_NAME).put_object(Body=json.dumps(card_list), Key='card_list.json',
-                                                    ContentType='json')
-    
-    return {'success': True, \
-        'message': f"card {card_id} successfully updated", \
-            'objects': card_list}
+                                                     ContentType='json')
 
+    return {'success': True,
+
+            "return_payload"{
+                'message': f"card {card_id} successfully updated", \
+                'objects': card_list
+                }
+            }
