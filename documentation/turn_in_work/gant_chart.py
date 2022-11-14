ec1da61b5f957c8b93462f0cbec5dffba819060f
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-11-13 17:24:30 -0900

    refactored gant chart

--- documentation/turn_in_work/gant_chart.py
+++ documentation/turn_in_work/gant_chart.py
@@ -4,15 +4,15 @@ import plotly.io as pio
 
 df = pd.DataFrame([
     dict(Task="Sprint 1 - Refactoring / Housekeeping", Start='2023-01-16', Finish='2023-01-31'),
-    dict(Task="Sprint 2 - Relational Database Migration", Start='2023-02-01', Finish='2023-02-28'),
-    dict(Task="Sprint 3 - Move to Permanent Hosting", Start='2023-03-01', Finish='2023-03-15'),
-    dict(Task="Sprint 4 - Image, Sound, and Video Feature", Start='2023-03-16', Finish='2023-03-31'),
-    dict(Task="Sprint 5 - Front-End Support for Images, Sounds, and Video", Start='2023-04-01', Finish='2023-04-15'),
-    dict(Task="Sprint 6 - Card Sharing Functionality", Start='2023-04-15', Finish='2023-04-22'),
-    dict(Task="Sprint 6.5 - Budgeted Refactoring Time", Start='2023-04-22', Finish='2023-04-30'),
-    dict(Task="Sprint 7 - Data Product Construction", Start='2023-05-01', Finish='2023-05-15'),
-    dict(Task="Sprint 8 - Maintenance / Cleaning / Documentation", Start='2023-05-15', Finish='2023-05-30'),
-    dict(Task="Total Time", Start='2023-01-01', Finish='2023-05-30'),
+    dict(Task="Sprint 2 - 3 - Relational Database Migration", Start='2023-02-01', Finish='2023-02-28'),
+    dict(Task="Sprint 4 - Move to Permanent Hosting", Start='2023-03-01', Finish='2023-03-15'),
+    dict(Task="Sprint 5 - Image, Sound, and Video Feature", Start='2023-03-16', Finish='2023-03-31'),
+    dict(Task="Sprint 6 - Front-End Support for Images, Sounds, and Video", Start='2023-04-01', Finish='2023-04-15'),
+    dict(Task="Sprint 7 - Card Sharing Functionality", Start='2023-04-15', Finish='2023-04-22'),
+    dict(Task="Sprint 7.5 - Budgeted Refactoring Time", Start='2023-04-22', Finish='2023-04-30'),
+    dict(Task="Sprint 8 - Data Product Construction", Start='2023-05-01', Finish='2023-05-15'),
+    dict(Task="Sprint 9 - Maintenance / Cleaning / Documentation", Start='2023-05-15', Finish='2023-05-30'),
+    dict(Task="Total Time", Start='2023-01-16', Finish='2023-05-30'),
 ])
 
 
