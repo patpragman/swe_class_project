655999edf03321266b09562c1dd9330ec3657760
Patrick Pragman <pat@pragman.io>
2022-11-23 16:04:32 -0900

    deleted unnecessary stuff

--- documentation/qrcode_gen.py
+++ documentation/qrcode_gen.py
@@ -1,4 +1,3 @@
 import qrcode
-img = qrcode.make('https://github.com/patpragman/swe_class_project/blob/master/documentation/systems.md')
-type(img)  # qrcode.image.pil.PilImage
-img.save("systems_qr_link.png")
+img = qrcode.make('https://github.com/patpragman/swe_class_project/blob/master/.github/workflows/push_to_dev.yml')
+img.save("yaml_link.png")
