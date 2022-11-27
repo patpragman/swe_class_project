c3b290f71a01edbae7ddc7c7fb20324ed3ac2f94
Patrick Pragman <pat@pragman.io>
2022-11-26 16:49:55 -0900

    trying my hand at a display function

--- docs/assets/js/login.js
+++ docs/assets/js/login.js
@@ -33,10 +33,69 @@ function login() {
         body: JSON.stringify(data)
     }).then(res => {
         console.log("Request complete! response:", res);
+        if (res['success']){
+            displayApplication(res)
+        }
     });
 
 
 }
+
+function cardFromObject(o){
+
+    /*
+    Example flashcard object!
+
+    {
+	"0": {
+		"owner": "patrick",
+		"back_image_path": "None",
+		"back_text": "test_card_back",
+		"folder": "test / not import",
+		"front_image_path": "None",
+		"streak": "0",
+		"id": 1,
+		"front_text": "test_card_front",
+		"create_date": "2022-11-21 01:12:11.247960",
+		"last_study_date": "2022-11-22 01:12:11.247970",
+		"next_study_due": "2022-11-26 01:12:11.247972"
+	}
+    }
+    */
+
+    // let's create a card
+    let card_div = document.createElement("div");
+    const id = o['id'];
+    card_div.className = "flashcard";
+
+    let title = document.createElement("h3");
+    title.innerText = "Flashcard!";
+
+    let front_text_tag = document.createElement('p');
+    front_text_tag.className = "front_text";
+    front_text_tag.innerText = o['front_text'];
+
+    let back_text_tag = document.createElement('p');
+
+    card_div.appendChild(title);
+    card_div.appendChild(front_text_tag);
+    card_div.appendChild(back_text_tag);
+
+    return card_div
+}
+
+
+function displayApplication(res){
+    const login_window = document.getElementById("login_container");
+    login_window.innerHTML = "";
+
+    for (o in res.return_payload){
+
+        login_window.appendChild(cardFromObject(o));
+
+    }
+}
+
 
 function createdNewUser() {
     const singupUsernameInput = document.getElementById("signupUsername")
