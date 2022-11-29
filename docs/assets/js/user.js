5a41a87c74480ed4f72840d0ace3a7150317b2ee
Jaaliyah <jaaliyah_alexander@outlook.com>
2022-11-29 03:28:56 -0900

    minor change

--- docs/assets/js/user.js
+++ docs/assets/js/user.js
@@ -18,11 +18,6 @@ function get_cards() {
     return JSON.parse(sessionStorage.getItem("return_payload")).objects;
 }
 
-//save a note to the local storage -> need to somehow send to lambda
-function save_cards(notes) {
-    localStorage.setItem("payload_return", JSON.stringify(cards));
-}
-
 //create new card html --> moving to another page?
 function create_card_element(id, front_text) {
 
@@ -36,34 +31,4 @@ function create_card_element(id, front_text) {
     element.placeholder = "Empty Flashcard";
 
     return element;
-}
-
-function add_card() {
-
-    const cards = get_cards();
-    const card_object = {
-        id: Math.floor(Math.random() * 100000),
-        content: ""
-    };
-
-    const card_element = create_card_element(card_object.id, card_object.front_text);
-    all_cards_container.insertBefore(card_element, add_card_button);
-
-    cards.push(card_object);
-    save_cards(cards);
-}
-
-function updateNote(id, newContent) {
-    const notes = getNotes();
-    const targetNote = notes.filter((note) => note.id == id)[0];
-
-    targetNote.content = newContent;
-    saveNotes(notes);
-}
-
-function deleteNote(id, element) {
-    const notes = getNotes().filter((note) => note.id != id);
-
-    saveNotes(notes);
-    notesContainer.removeChild(element);
 }
