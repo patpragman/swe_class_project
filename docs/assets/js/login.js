3c407491cb040dc56ca11ac2849757db96c66246
Jaaliyah <jaaliyah_alexander@outlook.com>
2022-12-01 06:44:33 -0900

    fixed delay between login/user page, finished create new user

--- docs/assets/js/login.js
+++ docs/assets/js/login.js
@@ -1,5 +1,7 @@
 const urlEndPoint = "https://c5j7g4oypbub6kqvkbdpmvhhra0njfgr.lambda-url.us-west-2.on.aws/"
 
+const loginForm = document.querySelector("#login");
+const createAccountForm = document.querySelector("#createAccount");
 
 function setLoginMessage(element, type, message) {
     const loginMessage = element.querySelector(".login__message");
@@ -9,10 +11,25 @@ function setLoginMessage(element, type, message) {
     loginMessage.classList.add(`login__message--${type}`); //type is success or error
 }
 
+async function fetch_login_json(data) {
+    const response = await fetch(urlEndPoint, {
+        method: "POST",
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+            //"Access-Control-Allow-Origin": 'https://patpragman.github.io/swe_class_project'
+        },
+        body: JSON.stringify(data)
+    })
 
-function login() {
+    const res_promise = await response.json()
+    return res_promise
+}
+
+async function login() {
     const usernameInput = document.getElementById("username")
     const passwordInput = document.getElementById("password")
+
 
     let data = {
         operation: "get_cards",
@@ -23,43 +40,34 @@ function login() {
         }
     };
 
-    fetch(urlEndPoint, {
-        method: "POST",
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-            //"Access-Control-Allow-Origin": 'https://patpragman.github.io/swe_class_project'
-        },
-        body: JSON.stringify(data)
-    }).then(res => {
-        const data = res.json();
-        return data
-    }).then(data => {
+    const res = await fetch_login_json(data)
 
-        if (data['success']) {
-            // if it was successful, run the function to launch the app
-            store_and_load_app(data['return_payload'], usernameInput.value, passwordInput.value);
-        }
+    if (res["success"]) {
+        console.log("passed")
+        store_info(res["return_payload"], usernameInput.value, passwordInput.value)
+        setLoginMessage(loginForm, "success", "Account Created!")
+        window.location.href = "user.html";
+
+    } else {
+        console.log("failed")
+        setLoginMessage(loginForm, "error", "Invalid username/password combination")
     }
-    );
-
 
 }
 
 
 
-function store_and_load_app(return_payload, username, password) {
+function store_info(return_payload, username, password) {
 
     // store the login login info and the cards and open the html that contains the app
 
     window.sessionStorage.setItem("username", username);
     window.sessionStorage.setItem("password", password);
     window.sessionStorage.setItem("return_payload", JSON.stringify(return_payload));
-    window.location.href = "user.html";
 
 }
 
-function createdNewUser() {
+async function createdNewUser() {
     const singupUsernameInput = document.getElementById("signupUsername")
     const signupPasswordInput = document.getElementById("signupPassword")
     const verifyPasswordInput = document.getElementById("verifyPassword")
@@ -80,24 +88,21 @@ function createdNewUser() {
                 password: "na",
                 object: user
             }
-
-
         };
 
-        fetch(urlEndPoint, {
-            method: "POST",
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                //"Access-Control-Allow-Origin": 'https://patpragman.github.io/swe_class_project'
-            },
-            body: JSON.stringify(data)
-        }).then(res => {
-            console.log("Request complete! response:", res);
-        });
+        const res = await fetch_login_json(data)
 
-        return true;
+        if (res["sucess"]) {
+            window.alert("account created!")
+            store_info({}, singupUsernameInput.value, signupPasswordInput.value)
+            setLoginMessage(createAccountForm, "success", "Account Created!")
+            window.location.href = "user.html";
+
+        } else {
+            window.alert("There was an error somehow.")
+        }
     } else {
+        setLoginMessage(createAccountForm, "error", "Passwords do not match, Try again!")
         signupPasswordInput.focus();
         return false;
     }
@@ -105,11 +110,8 @@ function createdNewUser() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
+        e.preventDefault(); // prevents redirect
         loginForm.classList.add("login--hidden");
         createAccountForm.classList.remove("login--hidden");
     });
@@ -123,27 +125,11 @@ document.addEventListener("DOMContentLoaded", () => {
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
 
-
-        if (login() == true) {
-            console.log(usernameInput.value, passwordInput.value)
-            setLoginMessage(createAccountForm, "success", "Account Created!")
-        }
-        else {
-            setLoginMessage(loginForm, "error", "Invalid username/password combination");
-        }
+        login()
     });
 
     createAccountForm.addEventListener("submit", e => {
         e.preventDefault();
-        if (createdNewUser() == false) {
-            setLoginMessage(createAccountForm, "error", "Passwords do not match, Try again!")
-
-
-        } else {
-            setLoginMessage(createAccountForm, "success", "Account Created!")
-        }
-
+        createdNewUser()
     })
-
-
 });
