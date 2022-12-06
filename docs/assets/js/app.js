3de7e9b9f05b406a46dd0608c4c6ddcd140a6b9e
Borogchingua <borogchingua@gmail.com>
2022-12-06 14:12:02 -0900

    panic attck

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -29,187 +29,182 @@ function shuffleCards(arr) {
     const x = arr.splice(i, 1)
     arr.sort(() => Math.random() - 0.5)
 
-    next_button.addEventListener("click", function () {
-        if (card_list.length > 0) {
-            i = Math.floor(Math.random() * card_list.length)
-            display_specific_card(card_list[i]["id"])
+    // add the current card back in at the top of the deck
+    arr.push(current_card_object)
+
+    return arr
+}
+
+//display
+function display_specific_card() {
+    // display the card at the top of the deck
+    current_card_object = card_list[card_list.length - 1]
+
+    // create the html for a new card
+    let old_object = prev_button.nextElementSibling
+    current_elem = create_card_element(current_card_object)
+    if (old_object != next_button) {
+        app_window.replaceChild(current_elem, old_object)
+    } else {
+        app_window.insertBefore(current_elem, next_button)
+    }
+}
+
+//create the flashcard
+function create_card_element(obj) {
+    const card_box = document.createElement("div");
+    const question = document.createElement('h2')
+    const answer = document.createElement("h2")
+
+
+    //const id = obj['id'];
+    card_box.classList.add("current_card")
+
+    question.setAttribute("style", "display:block")
+    question.id = "q"
+    question.textContent = obj['front_text']
+
+    answer.setAttribute("style", "display:none")
+    answer.id = "a"
+    answer.textContent = obj["back_text"]
+
+
+    card_box.appendChild(question)
+    card_box.appendChild(answer)
+
+    card_box.onclick = function () {
+        [question.style.display, answer.style.display] = [answer.style.display, question.style.display]
+    }
+
+
+    return card_box
+}
+
+/*
+Example flashcard object!
+
+{
+"0": {
+    "owner": "patrick",
+    "back_image_path": "None",
+    "back_text": "test_card_back",
+    "folder": "test / not import",
+    "front_image_path": "None",
+    "streak": "0",
+    "id": 1,
+    "front_text": "test_card_front",
+    "create_date": "2022-11-21 01:12:11.247960",
+    "last_study_date": "2022-11-22 01:12:11.247970",
+    "next_study_due": "2022-11-26 01:12:11.247972"
+}
+}
+*/
+
+// NOT FINISHED, STILL  NEED TO GET THE INPUT FROM TEXT BOXES
+function update_card() {
+    // get the q/a text
+    const ans = document.getElementById("a")
+    const qst = document.getElementById("q")
+
+    //make sure they're viewable
+    qst.setAttribute("style", "display:block")
+    ans.setAttribute("style", "display:block")
+
+    //add input boxes under q/a text
+    const answer_input_box = document.createElement("input")
+    answer_input_box.id = "answer"
+    answer_input_box.placeholder = "New Answer"
+    const question_input_box = document.createElement("input")
+    question_input_box.id = "question"
+    question_input_box.placeholder = "New Question"
+    const confirmation_button = document.createElement("button")
+    confirmation_button.textContent = "Ok"
+
+
+    ans.after(answer_input_box)
+    qst.after(question_input_box)
+    answer_input_box.after(confirmation_button)
+
+    confirmation_button.addEventListener("click", () => {
+        // update the current object text
+        current_card_object["front_text"] = question_input_box.value
+        current_card_object["back_text"] = answer_input_box.value
+        console.log(current_card_object["front_text"], current_card_object["back_text"])
+
+
+        let data = {
+            operation: "update_card",
+            payload: {
+                username: username,
+                password: password,
+                id: current_card_object["id"],
+                object: JSON.stringify(current_card_object)
+            }
+        };
+
+        fetch(urlEndPoint, {
+            method: "POST",
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                //"Access-Control-Allow-Origin": 'https://patpragman.github.io/swe_class_project'
+            },
+            body: JSON.stringify(data)
+        }).then(res => {
+            const data = res.json();
+            return data
+        }).then(data => {
+            if (data["sucess"]) {
+                window.alert("Card Updated")
+            }
+        });
+
+
+        display_specific_card()
+
+
+    })
+}
+
+function send_out(data) {
+
+
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    //button behavior
+    //new_card_button.addEventListener("click", () => add_card)
+    prev_button.addEventListener("click", function () {
+        if (viewed == 0) {
+            window.alert("This is the first viewed card today")
         } else {
-            window.alert("Congrats. You've reviewed all your cards.")
-            window.location.href = "user.html";
-
-            return arr
+            // remove the card on bottom of deck and move to the top
+            let p_card = card_list.shift()
+            card_list.push(p_card)
+            display_specific_card()
+            viewed = viewed - 1
         }
 
-        //display
-        function display_specific_card() {
-            // display the card at the top of the deck
-            current_card_object = card_list[card_list.length - 1]
+    })
 
-            // create the html for a new card
-            let old_object = prev_button.nextElementSibling
-            current_elem = create_card_element(current_card_object)
-            if (old_object != next_button) {
-                app_window.replaceChild(current_elem, old_object)
-            } else {
-                app_window.insertBefore(current_elem, next_button)
-            }
+    //displays a random unseen card or sends alert
+    next_button.addEventListener("click", function () {
+        if (viewed == card_list.length) {
+            window.alert("Congrats. You've reviewed all of your cards.")
+        } else {
+            // move the current card to the bottom of the deck
+            card_list.pop()
+            card_list.unshift(current_card_object)
+            display_specific_card()
+            viewed = viewed + 1
         }
 
-        //create the flashcard
-        function create_card_element(obj) {
-            const card_box = document.createElement("div");
-            const question = document.createElement('h2')
-            const answer = document.createElement("h2")
 
+    })
 
-            //const id = obj['id'];
-            card_box.classList.add("current_card")
-
-            question.setAttribute("style", "display:block")
-            question.id = "q"
-            question.textContent = obj['front_text']
-
-            answer.setAttribute("style", "display:none")
-            answer.id = "a"
-            answer.textContent = obj["back_text"]
-
-
-            card_box.appendChild(question)
-            card_box.appendChild(answer)
-
-            card_box.onclick = function () {
-                [question.style.display, answer.style.display] = [answer.style.display, question.style.display]
-            }
-
-
-            return card_box
-        }
-
-        /*
-        Example flashcard object!
-        
-        {
-        "0": {
-            "owner": "patrick",
-            "back_image_path": "None",
-            "back_text": "test_card_back",
-            "folder": "test / not import",
-            "front_image_path": "None",
-            "streak": "0",
-            "id": 1,
-            "front_text": "test_card_front",
-            "create_date": "2022-11-21 01:12:11.247960",
-            "last_study_date": "2022-11-22 01:12:11.247970",
-            "next_study_due": "2022-11-26 01:12:11.247972"
-        }
-        }
-        */
-
-        // NOT FINISHED, STILL  NEED TO GET THE INPUT FROM TEXT BOXES
-        function update_card() {
-            // get the q/a text
-            const ans = document.getElementById("a")
-            const qst = document.getElementById("q")
-
-            //make sure they're viewable
-            qst.setAttribute("style", "display:block")
-            ans.setAttribute("style", "display:block")
-
-            //add input boxes under q/a text
-            const answer_input_box = document.createElement("input")
-            answer_input_box.id = "answer"
-            answer_input_box.placeholder = "New Answer"
-            const question_input_box = document.createElement("input")
-            question_input_box.id = "question"
-            question_input_box.placeholder = "New Question"
-            const confirmation_button = document.createElement("button")
-            confirmation_button.textContent = "Ok"
-
-
-            ans.after(answer_input_box)
-            qst.after(question_input_box)
-            answer_input_box.after(confirmation_button)
-
-            confirmation_button.addEventListener("click", () => {
-                // update the current object text
-                current_card_object["front_text"] = question_input_box.value
-                current_card_object["back_text"] = answer_input_box.value
-                console.log(current_card_object["front_text"], current_card_object["back_text"])
-
-
-                let data = {
-                    operation: "update_card",
-                    payload: {
-                        username: username,
-                        password: password,
-                        id: current_card_object["id"],
-                        object: JSON.stringify(current_card_object)
-                    }
-                };
-
-                fetch(urlEndPoint, {
-                    method: "POST",
-                    mode: 'cors',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        //"Access-Control-Allow-Origin": 'https://patpragman.github.io/swe_class_project'
-                    },
-                    body: JSON.stringify(data)
-                }).then(res => {
-                    const data = res.json();
-                    return data
-                }).then(data => {
-                    if (data["sucess"]) {
-                        window.alert("Card Updated")
-                    }
-                });
-
-
-                display_specific_card()
-
-
-            })
-        }
-
-        function send_out(data) {
+    edit_button.addEventListener("click", update_card)
 
 
 
-        }
-
-        document.addEventListener("DOMContentLoaded", () => {
-            //button behavior
-            //new_card_button.addEventListener("click", () => add_card)
-            prev_button.addEventListener("click", function () {
-                if (viewed == 0) {
-                    window.alert("This is the first viewed card today")
-                } else {
-                    // remove the card on bottom of deck and move to the top
-                    let p_card = card_list.shift()
-                    card_list.push(p_card)
-                    display_specific_card()
-                    viewed = viewed - 1
-                }
-
-            })
-
-            //displays a random unseen card or sends alert
-            next_button.addEventListener("click", function () {
-                if (viewed == card_list.length) {
-                    window.alert("Congrats. You've reviewed all of your cards.")
-                } else {
-                    // move the current card to the bottom of the deck
-                    card_list.pop()
-                    card_list.unshift(current_card_object)
-                    display_specific_card()
-                    viewed = viewed + 1
-                }
-
-
-            })
-
-            edit_button.addEventListener("click", update_card)
-
-
-
-        })
+})
--- docs/assets/js/user.js
+++ docs/assets/js/user.js
@@ -1,39 +1,3 @@
-// const all_cards_container = document.getElementById("user_cards");
-// const add_card_button = all_cards_container.querySelector(".add-card");
-
-// get_cards().forEach((card) => {
-//     const card_element = create_card_element(card.id, card.front_text);
-//     all_cards_container.insertBefore(card_element, add_card_button);
-//     card_element.addEventListener("click", () => {
-//         window.sessionStorage.setItem("current_card", card.id)
-//         window.location.href = "app.html";
-//     })
-// });
-
-// add_card_button.addEventListener("click", () => addCard());
-
-
-// //get array of cards stored in local storage
-// function get_cards() {
-//     return JSON.parse(sessionStorage.getItem("return_payload")).objects;
-// }
-
-// //create new card html --> moving to another page?
-// function create_card_element(id, front_text) {
-
-
-//     const element = document.createElement("textarea");
-//     element.readOnly = true
-
-//     // add styling
-//     element.classList.add("card");
-//     element.value = front_text;
-//     element.placeholder = "Empty Flashcard";
-
-//     return element;
-// }
-
-////////// Add Flashcard pop up window /////////////
 const all_cards_container = document.getElementById("user_cards");
 const addQuestion = all_cards_container.querySelector(".add-card");
 const container = document.querySelector(".container");
@@ -44,6 +8,40 @@ const answer = document.getElementById("answer");
 const errorMessage = document.getElementById("error");
 const closeBtn = document.getElementById("close-btn");
 let editBool = false;
+
+get_cards().forEach((card) => {
+    const card_element = create_card_element(card.id, card.front_text);
+    all_cards_container.insertBefore(card_element, addQuestion);
+    card_element.addEventListener("click", () => {
+        window.sessionStorage.setItem("current_card", card.id)
+        window.location.href = "app.html";
+    })
+});
+
+addQuestion.addEventListener("click", () => addCard());
+
+
+//get array of cards stored in local storage
+function get_cards() {
+    return JSON.parse(sessionStorage.getItem("return_payload")).objects;
+}
+
+//create new card html --> moving to another page?
+function create_card_element(id, front_text) {
+
+
+    const element = document.createElement("textarea");
+    element.readOnly = true
+
+    // add styling
+    element.classList.add("card");
+    element.value = front_text;
+    element.placeholder = "Empty Flashcard";
+
+    return element;
+}
+
+////////// Add Flashcard pop up window /////////////
 
 //Add question when user clicks 'Add Flashcard' button
 addQuestion.addEventListener("click", () => {
@@ -71,90 +69,14 @@ cardButton.addEventListener(
     "click",
     (submitQuestion = () => {
         editBool = false;
-        tempQuestion = question.value.trim();
-        tempAnswer = answer.value.trim();
+        // tempQuestion = question.value.trim();
+        // tempAnswer = answer.value.trim();
         if (!tempQuestion || !tempAnswer) {
             errorMessage.classList.remove("hide");
         } else {
-            container.classList.remove("hide");
-            errorMessage.classList.add("hide");
-            viewlist();
-            question.value = "";
-            answer.value = "";
+            // viewlist();
+
         }
     })
 );
 
-//Card Generate
-function viewlist() {
-    var listCard = document.getElementsByClassName("card-list-container");
-    var div = document.createElement("div");
-    div.classList.add("card");
-    //Question
-    div.innerHTML += `
-    <p class="question-div">${question.value}</p>`;
-    //Answer
-    var displayAnswer = document.createElement("p");
-    displayAnswer.classList.add("answer-div", "hide");
-    displayAnswer.innerText = answer.value;
-
-    //Link to show/hide answer
-    var link = document.createElement("a");
-    link.setAttribute("href", "#");
-    link.setAttribute("class", "show-hide-btn");
-    link.innerHTML = "Show/Hide";
-    link.addEventListener("click", () => {
-        displayAnswer.classList.toggle("hide");
-    });
-
-    div.appendChild(link);
-    div.appendChild(displayAnswer);
-
-    //Edit button
-    let buttonsCon = document.createElement("div");
-    buttonsCon.classList.add("buttons-con");
-    var editButton = document.createElement("button");
-    editButton.setAttribute("class", "edit");
-    editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
-    editButton.addEventListener("click", () => {
-        editBool = true;
-        modifyElement(editButton, true);
-        addQuestionCard.classList.remove("hide");
-    });
-    buttonsCon.appendChild(editButton);
-    disableButtons(false);
-
-    //Delete Button
-    var deleteButton = document.createElement("button");
-    deleteButton.setAttribute("class", "delete");
-    deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-    deleteButton.addEventListener("click", () => {
-        modifyElement(deleteButton);
-    });
-    buttonsCon.appendChild(deleteButton);
-
-    div.appendChild(buttonsCon);
-    listCard[0].appendChild(div);
-    hideQuestion();
-}
-
-//Modify Elements
-const modifyElement = (element, edit = false) => {
-    let parentDiv = element.parentElement.parentElement;
-    let parentQuestion = parentDiv.querySelector(".question-div").innerText;
-    if (edit) {
-        let parentAns = parentDiv.querySelector(".answer-div").innerText;
-        answer.value = parentAns;
-        question.value = parentQuestion;
-        disableButtons(true);
-    }
-    parentDiv.remove();
-};
-
-//Disable edit and delete buttons
-const disableButtons = (value) => {
-    let editButtons = document.getElementsByClassName("edit");
-    Array.from(editButtons).forEach((element) => {
-        element.disabled = value;
-    });
-};
